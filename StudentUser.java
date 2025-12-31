459d79ba879aec9b846d2437378c232918358cb3
User <abdrahmanalmuhan@gmail.com>
2025-12-31 13:15:50 +0500

    assignment 2

---
+++ StudentUser.java
@@ -0,0 +1,12 @@
+public class StudentUser extends LibraryUser {
+
+    public StudentUser(String name, int userId) {
+        super(name, userId);
+    }
+
+    @Override
+    public void borrowBook(Book book) {
+        System.out.println("student " + name + " borrowed " + book.getTitle());
+        book.setBorrowed(true);
+    }
+}
